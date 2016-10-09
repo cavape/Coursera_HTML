72b76c4d0ddcee954f7c15cb63ae09375261fed8
Your NameCarlos <carlos@Carloss-MacBook-Pro.local>
2016-10-09 07:38:32 -0400

    small addition

---
+++ Coursera_JavaScript/form/form.js
@@ -0,0 +1,10 @@
+function billingFunction(){
+  if(same.checked==true){
+    billingName.value=shippingName.value;
+    billingZip.value=shippingZip.value;
+  }
+  else{
+    billingName.value="";
+    billingZip.value="";
+  }
+};
